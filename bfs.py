8b4aa6d386824ad89803589f331a2a1e856bca4f
BedirYilmaz <BedirYilmaz@github.com>
2024-01-26 08:03:35 +0100

    add iterative version

--- bfs.py
+++ bfs.py
@@ -20,12 +20,29 @@ def bfs (edge):
     print(edge)
     for i in range(len(graph[edge])):
         que.put(graph[edge][i])
-    bfs(que.get())
-    
-print("******* breadth first search ******* \n")
+    if not que.empty():
+        bfs(que.get())
+
+def bfs_iterative (edge):
+    que.put(edge)
+
+    discovered = []
+    while not que.empty():
+        node = que.get()
+        if node not in discovered:
+            for neighbor in graph[node]:
+                que.put(neighbor)
+            discovered.append(node)
+            print(node)
+
+
+print("******* breadth first search   ******* \n")
+print("\n*******       recursive      ******* \n")
 que = queue.SimpleQueue()
 bfs("A")
-
+print("\n*******       iterative      ******* \n")
+que = queue.SimpleQueue()
+bfs_iterative("A")
 
 
 
